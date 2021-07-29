2c0d210b9196f836f5136f9f6adb0f55e264ce8f
Chris <hargrove.chris91@gmail.com>
2021-07-29 16:29:50 +0100

    feat: created serializable stack property drawer

---
+++ Editor/Serializable Stack/SerializableStackPropertyDrawer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace BatteryAcid.Serializables.Editor
+{
+    [CustomPropertyDrawer(typeof(SerializableStackBase), true)]
+    public class SerializableStackPropertyDrawer : PropertyDrawer
+    {
+        private SerializedProperty Values { get; set; }
+        private ReorderableList Reorderable { get; set; }
+
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            float totalHeight = EditorGUIUtility.singleLineHeight;
+            if (Reorderable != null)
+            {
+                totalHeight = Reorderable.GetHeight();
+            }
+            return totalHeight;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Values = property.FindPropertyRelative("values");
+            if (Reorderable == null)
+            {
+                Reorderable = new ReorderableList(property.serializedObject, Values, true, true, true, true);
+                Reorderable.drawHeaderCallback = (Rect rect) => EditorGUI.LabelField(rect, label);
+                Reorderable.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+                {
+                    SerializedProperty element = Reorderable.serializedProperty.GetArrayElementAtIndex(index).FindPropertyRelative("Value");
+                    Rect betterRect = new Rect(rect.xMin, rect.yMin + 2.5f, rect.width, rect.height);
+                    EditorGUI.PropertyField(betterRect, element, GUIContent.none, true);
+                };
+                Reorderable.onAddCallback = (ReorderableList list) => list.serializedProperty.arraySize++;
+                Reorderable.elementHeightCallback = (int index) =>
+                {
+                    SerializedProperty property = Values.GetArrayElementAtIndex(index).FindPropertyRelative("Value");
+                    float expandedOffset = property.isExpanded ? 5f : 0f;
+                    return EditorGUI.GetPropertyHeight(property, true) + expandedOffset;
+                };
+            }
+            Reorderable.serializedProperty = Values;
+            Reorderable.DoList(position);
+        }
+    }
+}
