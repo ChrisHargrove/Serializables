43a1a99d902658abeeb46efe58acd2b9b9d3d093
Chris <hargrove.chris91@gmail.com>
2021-07-29 20:03:39 +0100

    refactor: moved common serializable editor functionality

---
+++ Editor/Common/SerializablePropertyId.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+
+namespace BatteryAcid.Serializables.Editor
+{
+    /// <summary>
+    /// Class that helps with using a SerializedProperty as a key value in a dictionary.
+    /// </summary>
+    public struct SerializablePropertyId
+    {
+        public SerializablePropertyId(SerializedProperty property)
+        {
+            Instance = property.serializedObject.targetObject;
+            Path = property.propertyPath;
+        }
+        private readonly UnityEngine.Object Instance;
+        private readonly string Path;
+    }
+}
