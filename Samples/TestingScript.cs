54cec396f8b298e88f6412be3709d78771f6d52d
Chris Hargrove <hargrove.chris91@gmail.com>
2023-03-21 09:19:56 +0000

    chore: changes to visualize the new datetime field in the testing script

--- Samples/TestingScript.cs
+++ Samples/TestingScript.cs
@@ -6,14 +6,6 @@ using UnityEngine;
 public class TestingScript : MonoBehaviour
 {
     [SerializeField]
-    private SerializableGuid first;
-    private SerializableGuid First => first;
-
-    [SerializeField]
-    private SerializableGuid second;
-    private SerializableGuid Second => second;
-
-    [SerializeField]
-    private List<SerializableGuid> guids;
-    private List<SerializableGuid> Guids => guids;
+    private SerializableDateTime testDate;
+    private SerializableDateTime TestDate => testDate;
 }
