2aae7ca0a2e20f9524175e201058cc011e76790f
Chris Hargrove <hargrove.chris91@gmail.com>
2023-03-22 08:56:52 +0000

    chore: added a months enum for the custom window to use

---
+++ Editor/Serializable DateTime/SerializableDateTimeMonths.cs
@@ -0,0 +1,18 @@
+namespace BatteryAcid.Serializables.Editor
+{
+    internal enum Months
+    {
+        January = 1,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
