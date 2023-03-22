aa35ee58ccb40e1b8141f8b87aa6edd4397f8bc5
Chris Hargrove <hargrove.chris91@gmail.com>
2023-03-22 08:56:35 +0000

    chore: added default color values for fields and the query string for the visual element

--- Editor/Serializable DateTime/SerializableDateTimeCustomWindow.cs
+++ Editor/Serializable DateTime/SerializableDateTimeCustomWindow.cs
@@ -1,29 +1,47 @@
 using System;
 using UnityEditor;
 using UnityEditor.UIElements;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace BatteryAcid.Serializables.Editor
 {
     internal class SerializableDateTimeCustomWindow : UnityEditor.EditorWindow
     {
+        private const string InputQuery = "unity-text-input";
+        private Color DefaultTopColor => new Color(0.05098039f, 0.05098039f, 0.05098039f);
+        private Color DefaultLeftColor => new Color(0.1294118f, 0.1294118f, 0.1294118f);
+        private Color DefaultRightColor => new Color(0.1294118f, 0.1294118f, 0.1294118f);
+        private Color DefaultBottomColor => new Color(0.1294118f, 0.1294118f, 0.1294118f);
+
         public void OnEnable()
         {
             VisualElement container = new VisualElement();
 
             Year = new IntegerField("Year")
                 .FlexGrow(true);
-            Month = new IntegerField("Month")
-                .FlexGrow(true);
+            Month = new EnumField("Month", Months.January)
+                .FlexGrow(true)
+                .SetOnValueChanged((Enum newValue) => ValidateFields());
             Day = new IntegerField("Day")
-                .FlexGrow(true);
+                .FlexGrow(true)
+                .SetOnValueChanged((int newValue) => ValidateFields());
+            DayInput = Day.Q<VisualElement>(InputQuery);
 
             Hour = new IntegerField("Hour")
-                .FlexGrow(true);
+                .FlexGrow(true)
+                .SetOnValueChanged((int newValue) => ValidateFields());
+            HourInput = Hour.Q<VisualElement>(InputQuery);
+
             Minute = new IntegerField("Minute")
-                .FlexGrow(true);
+                .FlexGrow(true)
+                .SetOnValueChanged((int newValue) => ValidateFields());
+            MinuteInput = Minute.Q<VisualElement>(InputQuery);
+
             Second = new IntegerField("Second")
-                .FlexGrow(true);
+                .FlexGrow(true)
+                .SetOnValueChanged((int newValue) => ValidateFields());
+            SecondInput = Second.Q<VisualElement>(InputQuery);
 
             Kind = new EnumField("Kind", DateTimeKind.Local)
                 .FlexGrow(true);
@@ -59,11 +77,15 @@ namespace BatteryAcid.Serializables.Editor
         }
 
         private IntegerField Year { get; set; }
-        private IntegerField Month { get; set; }
+        private EnumField Month { get; set; }
         private IntegerField Day { get; set; }
+        private VisualElement DayInput { get; set; }
         private IntegerField Hour { get; set; }
+        private VisualElement HourInput { get; set; }
         private IntegerField Minute { get; set; }
+        private VisualElement MinuteInput { get; set; }
         private IntegerField Second { get; set; }
+        private VisualElement SecondInput { get; set; }
         private EnumField Kind { get; set; }
 
         public Action<DateTime> OnConfirmation { get; set; }
@@ -72,7 +94,7 @@ namespace BatteryAcid.Serializables.Editor
         {
             if (ValidateFields())
             {
-                DateTime newDateTime = new DateTime(Year.value, Month.value, Day.value, Hour.value, Minute.value, Second.value, (DateTimeKind)Kind.value);
+                DateTime newDateTime = new DateTime(Year.value, (int)(Months)Month.value, Day.value, Hour.value, Minute.value, Second.value, (DateTimeKind)Kind.value);
                 OnConfirmation?.Invoke(newDateTime);
                 this.Close();
             }
@@ -85,8 +107,34 @@ namespace BatteryAcid.Serializables.Editor
 
         private bool ValidateFields()
         {
-            //TODO: Validate datetime fields to stop incorrect dates being created.
-            return true;
+            bool isValid = true;
+            int allowedDays = 0;
+            allowedDays = (Months)Month.value switch
+            {
+                Months.February => 28,
+                Months.April or Months.June or Months.September or Months.November => 30,
+                _ => 31,
+            };
+
+            void validateField(IntegerField valueField, VisualElement borderField, int min, int max)
+            {
+                if (valueField.value < min || valueField.value > max)
+                {
+                    borderField.SetBorderColor(Color.red);
+                    isValid = false;
+                }
+                else
+                {
+                    borderField.SetBorderColor(DefaultTopColor, DefaultRightColor, DefaultBottomColor, DefaultLeftColor);
+                }
+            }
+
+            validateField(Day, DayInput, 1, allowedDays);
+            validateField(Hour, HourInput, 0, 23);
+            validateField(Minute, MinuteInput, 0, 59);
+            validateField(Second, SecondInput, 0, 59);
+
+            return isValid;
         }
     }
 }
