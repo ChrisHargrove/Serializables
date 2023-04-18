ac105e1118f885a12bac91ad444e4682bb85ac80
Chris Hargrove <hargrove.chris91@gmail.com>
2023-04-18 10:51:00 +0100

    chore: added new tracking variables to the test script to test them.

--- Samples/TestingScript.cs
+++ Samples/TestingScript.cs
@@ -16,4 +16,12 @@ public class TestingScript : MonoBehaviour
     [SerializeField]
     private SerializableGuid testGuid;
     private SerializableGuid TestGuid => testGuid;
+
+    [SerializeField]
+    private SerializableStack<float> testFloatStack;
+    private SerializableStack<float> TestFloatStack => testFloatStack;
+
+    [SerializeField]
+    private SerializableStack<SerializableGuid> testGuidStack;
+    private SerializableStack<SerializableGuid> TestGuidStack => testGuidStack;
 }
